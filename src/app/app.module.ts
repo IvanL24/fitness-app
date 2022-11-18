201045f09503dc9fe3625ab6daae91a6d2f84f57
IvanL24 <lavenaivan@gmail.com>
2022-11-18 00:31:22 -0700

    basic design plan implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    TasksComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
